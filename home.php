e9ae2b9f6d44b246f97c8cb3bb5f20c37cd8acf0
thunsitaKensom <thunsita.k@kkumail.com>
2019-03-30 23:33:27 +0700

    big fuck bug fix / fixed wrong sql query

--- home.php
+++ home.php
@@ -34,7 +34,7 @@ session_start(); ?>
                 <?php 
                 $setColumn = 3;
                 $ColumnStart = 1;
-                $stmt = $pdo->prepare("SELECT * FROM subject,user_role,user_info where user_info.UID = user_role.UID AND user_info.UID = ?");
+                $stmt = $pdo->prepare("SELECT * FROM subject,user_role,user_info where user_info.UID = user_role.UID AND user_role.subject_id = subject.subject_id  AND user_info.UID = ?");
                 $stmt->bindParam(1, $_SESSION["UID"]);
                 $stmt->execute();
                 while ($row = $stmt->fetch()) {
