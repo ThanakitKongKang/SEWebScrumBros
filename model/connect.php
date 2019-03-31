4e93b267edbead2f6981e245929cf3aac137006f
thunsitaKensom <thunsita.k@kkumail.com>
2019-03-31 14:43:43 +0700

    change dbname

--- model/connect.php
+++ model/connect.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=sescrumbros;charset=utf8", "root", "") or die("Error: " . mysqli_error($pdo));
+$pdo = new PDO("mysql:host=localhost;dbname=19s2_g7;charset=utf8", "root", "") or die("Error: " . mysqli_error($pdo));
 date_default_timezone_set('asia/bangkok'); ?>
