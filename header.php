dcc540fbfc2ee7b109a29b5498eaccde2fa12b81
kchaiwat <k.chaiwat@kkumail.com>
2019-04-02 20:49:20 +0700

    add name in logout button

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 
         <?php if (!empty($_SESSION["firstName"])) { ?>
         <span class="navbar-text fsize text-primary"><?= $_SESSION["firstName"]," ",$_SESSION["lastName"] ?> </span>
-        <a href="logout.php"><button class="btn btn-danger mx-3" type="button">ออกจากระบบ</button></a>
+        <a href="logout.php"><button class="btn btn-danger mx-3" type="button" name="logout">ออกจากระบบ</button></a>
 
         <?php 
     } ?>
